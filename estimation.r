3fd4980d1305e424ba72f266ccf9ac5d62ed1a78
David Gold <david.gold.a@gmail.com>
2017-10-14 17:01:06 -0700

    Scripts

---
+++ estimation.r
@@ -0,0 +1,235 @@
+# setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+#########################################################################
+# Dependencies
+
+# library(purrr)
+# library(glmnet)
+
+#########################################################################
+# Tuning parameter
+
+.Lambdahat <- function(X, t = .05, m = 500){
+  n <- nrow(X)
+  g <- rnorm(n * m, 0, 1)
+  map(1:m, ~ (X * rnorm(n, 0, 1)) %>%
+        apply(2, sum) %>%
+        max) %>%
+    as.numeric %>%
+    quantile(1 - t)
+}
+
+.lambda <- function(X, sigma0hat, t = 0.05, c = 1.1) {  2 * c * sigma0hat * .Lambdahat(X, t = t) / nrow(X) }
+
+.sigma0_u.hat <- function(y, x, beta_est) { sd(y - x %*% beta_est) }
+
+#########################################################################
+# Estimation
+
+# First-stage
+.lambda_j.Alpha0hat <- function(X, Z, sigma0_v, tune_type = "CV"){
+  px <- ncol(X); pz <- ncol(Z)
+  if ( tune_type == "CV") {
+    # fits <- map(1:px, ~ cv.glmnet(Z, X[,.], intercept = FALSE, nfolds = 5)) %>%
+    # ncores <- 100
+    # cl <- makeCluster(ncores, type="FORK")
+    # fits <- parLapply(cl, 1:px, function(j) cv.glmnet(Z, X[,j], intercept=F, nfolds=5))
+    # stopCluster(cl)
+    fits <- lapply(1:px, function(j) cv.glmnet(Z, X[,j], intercept=F, nfolds=5))
+    fits <- fits %>%
+      map(~ list(lambda_j = .$lambda.min,
+                 alpha0_jhat = coef(., s = "lambda.min")))
+  } else {
+    if ( tune_type == "semioracle" ) {
+      sigma0_vhat <- sigma0_v
+    } else if ( tune_type == "feasible" ) {
+      # ...
+    }
+    lambda <- .lambda(Z, sigma0_vhat)
+    fits <- map(1:px, ~ list(X_j = X[,.], fit = glmnet(Z, X[,.], intercept = FALSE))) %>%
+      map(~ list(lambda_j = lambda,
+                 alpha0_jhat = predict(.$fit, type = "coefficients",
+                                       s = lambda, exact = TRUE, x = Z, y = .$X_j)))
+  }
+  Alpha0hat <- map(fits, ~ as.numeric(.$alpha0_jhat)[2:(pz+1)]) %>%
+    reduce(cbind) %>% { colnames(.) <- NULL; . }
+  list(lambda_j = map_dbl(fits, ~ .$lambda_j), Alpha0hat = Alpha0hat)
+}
+
+# Second-stage
+.lambda.beta_Lasso <- function(y, Dhat, sigma0_h, tune_type = "CV", no_pen_ids = c()) {
+  px <- ncol(Dhat)
+  if ( length(no_pen_ids)==0 ) {
+    penalties <- rep(1, px)
+  } else {
+    penalties <- rep(1, px); penalties[no_pen_ids] <- 0
+  }
+  if ( tune_type == "CV") {
+    res <- cv.glmnet(Dhat, y, intercept = FALSE, standardize=F, nfolds = 5, penalty.factor=penalties) %>%
+    { list(lambda = .$lambda.min,
+           beta_Lasso = as.numeric(coef(., s = "lambda.min"))[2:(px+1)]) }
+  } else {
+    if ( tune_type == "infeasible" ) {
+      sigma0_u.hat <- sigma0_h
+    } else if ( tune_type == "feasible" ) {
+      # ...
+    }
+    lambda <- .lambda(Dhat, sigma0_u.hat)
+    res <- glmnet(Dhat, y, intercept = FALSE, penalty.factor=penalties) %>%
+    { list(lambda = lambda,
+           beta_Lasso = as.numeric(predict(., type = "coefficients", s = lambda,
+                                           exact = TRUE, x = Dhat, y = y))[2:(px+1)]) }
+  }
+  res
+}
+
+# Empirical Gram matrix
+.Sigmahat <- function(x) { (t(x) %*% x) / nrow(x) }
+
+find_mu <- function(Sigma.hat) {
+  px <- ncol(Sigma.hat)
+  c1 <- lapply(1:px,
+               function(j) { a <- rep(0,px); a[j] <- -1; c(-1*Sigma.hat[j,], a, 0) }) %>%
+    reduce(rbind)
+  c2 <- lapply(1:px,
+               function(j) { a <- rep(0,px); a[j] <- -1; c(Sigma.hat[j,], a, 0) }) %>%
+    reduce(rbind)
+  c3_1 <- matrix(ncol=px,nrow=px); c3_1[,] <- 0
+  c3 <- cbind(c3_1, diag(1, px), rep(-1, px))
+  A <- rbind(c1, c2, c3)
+  obj <- c(rep(0, 2*px), 1)
+  dir <- rep("<=", 3*px)
+  mus <- lapply(
+    1:px,
+    function(j) {
+      b1.j <- rep(0,px); b1.j[j] <- -1
+      b2.j <- rep(0,px); b2.j[j] <- 1
+      b3.j <- rep(0,px)
+      b.j <- c(b1.j, b2.j, b3.j)
+      res.j <- lp(direction="min", objective.in=obj, const.mat=A, const.dir=dir, const.rhs=b.j)
+      res.j$objval
+    }
+  ) %>% as.numeric
+
+  # ncores <- 100
+  # cl <- makeCluster(ncores, type="FORK")
+  # c1 <- parLapply(cl, 1:px,
+  #                  function(j) { a <- rep(0,px); a[j] <- -1; c(-1*Sigma.hat[j,], a, 0) }) %>%
+  #   reduce(rbind)
+  # c2 <- parLapply(cl, 1:px,
+  #                  function(j) { a <- rep(0,px); a[j] <- -1; c(Sigma.hat[j,], a, 0) }) %>%
+  #   reduce(rbind)
+  # mus <- parLapply(
+  #   cl, 1:px,
+  #   function(j) {
+  #     b1.j <- rep(0,px); b1.j[j] <- -1
+  #     b2.j <- rep(0,px); b2.j[j] <- 1
+  #     b3.j <- rep(0,px)
+  #     b.j <- c(b1.j, b2.j, b3.j)
+  #     res.j <- lp(direction="min", objective.in=obj, const.mat=A, const.dir=dir, const.rhs=b.j)
+  #     res.j$objval
+  #   }
+  # ) %>% as.numeric
+  # stopCluster(cl)
+
+
+  mus
+}
+
+.Theta.hat_JM <- function(Sigma.hat, n) {
+  Theta.hat <- InverseLinfty(Sigma.hat, n)
+  Theta.hat
+}
+
+
+.Theta.hat_CLIME <- function(Sigma.hat, mus) {
+  px <- ncol(Sigma.hat)
+  # ncores <- 100
+  # cl <- makeCluster(ncores, type="FORK")
+  # c1 <- parLapply(cl, 1:px,
+  #                 function(j) { a <- rep(0,2*px); a[j] <- -1; a[j+px] <- -1; a } ) %>%
+  #   reduce(rbind)
+  # c2 <- parLapply(cl, 1:px,
+  #                 function(j) { a <- rep(0,2*px); a[j] <- 1; a[j+px] <- -1; a } ) %>%
+  #   reduce(rbind)
+  # c3 <- parLapply(cl, 1:px,
+  #                 function(j) { c(-1*Sigma.hat[j,], rep(0, px)) }) %>%
+  #   reduce(rbind)
+  # c4 <- parLapply(cl, 1:px,
+  #                 function(j) { c(Sigma.hat[j,], rep(0, px)) }) %>%
+  #   reduce(rbind)
+  c1 <- lapply(1:px,
+               function(j) { a <- rep(0,2*px); a[j] <- -1; a[j+px] <- -1; a } ) %>%
+    reduce(rbind)
+  c2 <- lapply(1:px,
+               function(j) { a <- rep(0,2*px); a[j] <- 1; a[j+px] <- -1; a } ) %>%
+    reduce(rbind)
+  c3 <- lapply(1:px,
+               function(j) { c(-1*Sigma.hat[j,], rep(0, px)) }) %>%
+    reduce(rbind)
+  c4 <- lapply(1:px,
+               function(j) { c(Sigma.hat[j,], rep(0, px)) }) %>%
+    reduce(rbind)
+  A <- rbind(c1, c2, c3, c4)
+  dir <- rep("<=", 4*px)
+  obj <- c(rep(0,px), rep(1,px))
+
+  # Theta.hat <- parLapply(
+  #   cl, 1:px,
+  #   function(j) {
+  #     b1 <- rep(mus[j], px); b1[j] = mus[j]-1
+  #     b2 <- rep(mus[j], px); b2[j] = mus[j]+1
+  #     b <- c(rep(0, 2*px), b1, b2)
+  #     res.j <- lp(direction="min", objective.in=obj, const.mat=A, const.dir=dir, const.rhs=b)
+  #     res.j$solution[1:px]
+  #   }
+  # ) %>%
+  #   reduce(rbind)
+  # stopCluster(cl)
+  Theta.hat <- lapply(
+    1:px,
+    function(j) {
+      mu <- mus[j]
+      b1 <- rep(mu, px); b1[j] = mu-1
+      b2 <- rep(mu, px); b2[j] = mu+1
+      b <- c(rep(0, 2*px), b1, b2)
+      res.j <- lp(direction="min", objective.in=obj, const.mat=A, const.dir=dir, const.rhs=b)
+      res.j$solution[1:px]
+    }
+  ) %>%
+    reduce(rbind)
+  Theta.hat
+}
+
+# De-biased second-stage Lasso estimation
+
+.beta_debiased <- function(y, X, Dhat, beta_Lasso, Thetahat) {
+  n <- length(y)
+  lambda_kappahat <- t(Dhat) %*% (y - Dhat %*% beta_Lasso) / n
+  beta_debiased <- beta_Lasso + Thetahat %*% lambda_kappahat + (Thetahat %*% t(Dhat) %*% (Dhat - X) %*% beta_Lasso)/n
+  beta_debiased
+}
+
+#########################################################################
+# Standard errors
+
+# SE1
+# This is given by sigma_u.hat * theta_j^T * Sigma_d.hat * theta_j
+.SE1 <-function(Sigma_d.hat, Theta.hat, sd_u.hat) {
+  sd_u.hat * (Theta.hat %*% Sigma_d.hat %*% t(Theta.hat)) %>% diag %>% sqrt
+}
+
+# SE2
+# This is given by E_n[{theta_j, d_i}^2u_i^2] %>% sqrt
+.SE2 <- function(D.hat, Theta.hat, u.hat) {
+  # n <- length(u.hat); p <- ncol(D.hat)
+  (Theta.hat %*% t(D.hat)) %>%
+    apply(1, function(X) { mean(X^2 * u.hat^2) }) %>%
+    sqrt
+}
+
+# SE3
+# This is given by sigma_u.hat *
+.SE3 <- function(Theta.hat, sd_u.hat) {
+  sqrt(diag(Theta.hat)) * sd_u.hat
+}
---
+++ hdiv2e.r
@@ -0,0 +1,239 @@
+##two exogenous variables###
+
+library(purrr)
+library(mvtnorm)
+library(glmnet)
+library(dplyr)
+source("estimation.r")
+source("utils.r")
+
+##########config###############
+
+trial <- function() {
+  args = commandArgs(trailingOnly=TRUE)
+  config_id_ <- args[1] %>% as.numeric
+  trial_id <- Sys.getenv('SLURM_ARRAY_TASK_ID') %>% as.numeric
+  configs <- read.csv("configs.csv")
+  config <- filter(configs, config_id == config_id_)
+  T <- config$T; J <- config$J; L <- config$L
+  set.seed(trial_id)
+  nn=1000
+  TJ=T*J
+
+  sigmaxi=0.5
+  xi=rnorm(TJ)*sigmaxi
+  betatrue=c(-1,1) # true mean tastes
+  K=length(betatrue) # # of attributes
+  covX=matrix(c(1,0.5,0.5,1),K,K)
+  x=matrix(rnorm(TJ*K),TJ,K)%*%(covX) # products attributes TJ by dim(beta)
+  delta=x%*%betatrue+xi #TJ by 1
+
+  v=matrix(rnorm(nn),K,nn) #K by nn
+  covrc=diag(.5,K,K)
+  rc=t(chol(covrc))%*%v # K by nn
+  MU=x%*%rc #TJ by nn
+
+  numer=exp(kronecker(matrix(1,1,nn),delta)+MU) #numerator is a TJ by nn matrix
+  tem=cbind(seq(TJ),kronecker(seq(J),matrix(1,T,1)),kronecker(matrix(1,J,1),seq(T))) #TJ by dim(beta)
+  datasort=tem[order(tem[,3],tem[,2])]
+  a=numer[datasort,]
+  temp=apply(a,2,cumsum) #TJ by nn
+  temp1=temp[c((1:T)*J),] #T by nn
+  temp1[c(2:nrow(temp1)),]=diff(temp1)
+
+  denom=1+temp1 #T by nn
+  temp2=kronecker(matrix(1,J,1),denom)
+  s=rowMeans(numer/temp2) #T*J by 1: averaging over nn
+  s0=rowMeans(1/denom) #T by 1: averaging over nn
+
+  sum(s)+sum(s0)
+  min(s)
+  max(s)
+
+  R=log(s)-log(kronecker(matrix(1,J,1),s0))
+  sMatrix=matrix(s,nrow=T,ncol=J)
+  x1Mat<-t(matrix(x[,1],nrow=T,ncol=J))
+  x2Mat<-t(matrix(x[,2],nrow=T,ncol=J))
+  share<-t(sMatrix) #J by T
+
+
+  dd1=array(NA,c(J,T,J))
+  dz1=dd1
+  a1=matrix(NA,J,T)
+  D1=matrix(0,(J*J*T),1)
+  Dz1=D1
+
+  dd2=array(NA,c(J,T,J))
+  dz2=dd2
+  a2=matrix(NA,J,T)
+  D2=matrix(0,(J*J*T),1)
+  Dz2=D2
+
+
+  ss=array(NA,c(J,T,J))
+  sss=matrix(NA,J,T)
+  S=matrix(0,(J*J*T),1)
+
+  S0matrix=t(kronecker(matrix(1,1,J),s0))
+
+  ss0=array(NA,c(J,T,J))
+  sss0=matrix(NA,J,T)
+  S0=matrix(0,(J*J*T),1)
+
+  for (j in 1:J){
+    for (t in 1:T){
+      sss[,t]=share[j,t]
+      ss[,,j]=sss
+      S[((j-1)*TJ+1):(j*(TJ))]=as.vector(ss[,,j])
+
+      sss0[,t]=S0matrix[j,t]
+      ss0[,,j]=sss0
+      S0[((j-1)*TJ+1):(j*(TJ))]=as.vector(ss[,,j])
+
+      a1[,t]=(x1Mat[j,t]-x1Mat[,t])/share[j,t]
+      dd1[,,j]=a1
+      D1[((j-1)*TJ+1):(j*(TJ))]=as.vector(dd1[,,j])
+
+      a1[,t]=(x1Mat[j,t]-x1Mat[,t])/share[j,t]
+      dd1[,,j]=a1
+      dz1[,,j]=(x1Mat[j,t]-x1Mat[,t])
+      D1[((j-1)*TJ+1):(j*(TJ))]=as.vector(dd1[,,j])
+      Dz1[((j-1)*TJ+1):(j*(TJ))]=as.vector(dz1[,,j])
+
+
+      a2[,t]=x2Mat[j,t]-x2Mat[,t]/share[j,t]
+      dd2[,,j]=a2
+      dz2[,,j]=(x2Mat[j,t]-x2Mat[,t])
+      D2[((j-1)*TJ+1):(j*(TJ))]=as.vector(dd2[,,j])
+      Dz2[((j-1)*TJ+1):(j*(TJ))]=as.vector(dz2[,,j])
+
+    }
+  }
+
+  D=cbind(S*Dz1,S*Dz2) #TJ*J by 1
+
+
+  Dz=cbind(Dz1,Dz2)
+
+  dd<-polym(D[,1],D[,2],degree=L,raw=TRUE)
+  ddz<-polym(Dz[,1],Dz[,2],degree=L,raw=TRUE)
+
+
+  basisD=matrix(NA,TJ,length(dd[1,]))
+  basisDz=matrix(NA,TJ,length(ddz[1,]))
+
+  for (t in 1:TJ){
+    basisD[t,]=colSums(dd[(t*J-J+1):(t*J),]) #basisX is TJ by L
+    basisDz[t,]=colSums(ddz[(t*J-J+1):(t*J),]) #basisX is TJ by L
+  }
+
+
+  ######
+  dd1=array(NA,c(J,T,J))
+  a1=matrix(NA,J,T)
+  bX1=matrix(0,(J*J*T),1)
+  bXz1=bX1
+
+  dd2=array(NA,c(J,T,J))
+  dd1z=dd1
+  dd2z=dd2
+  a2=matrix(NA,J,T)
+  bX2=matrix(0,(J*J*T),1)
+  bXz2=bX2
+
+  for (j in 1:J){
+    for (t in 1:T){
+      a1[,t]=(x1Mat[,t])/s0[t]
+      a1[,t]=x1Mat[,t]
+      dd1[,,j]=a1
+      dd1z[,,j]=x1Mat[,t]
+      bX1[((j-1)*TJ+1):(j*(TJ))]=as.vector(dd1[,,j])
+      bXz1[((j-1)*TJ+1):(j*(TJ))]=as.vector(dd1z[,,j])
+
+      a2[,t]=x2Mat[,t]/s0[t]
+      a2[,t]=x2Mat[,t]
+      dd2[,,j]=a2
+      dd2z[,,j]=x2Mat[,t]
+      bX2[((j-1)*TJ+1):(j*(TJ))]=as.vector(dd2[,,j])
+      bXz2[((j-1)*TJ+1):(j*(TJ))]=as.vector(dd2z[,,j])
+    }
+  }
+
+
+  bX=cbind(S0*bXz1,S0*bXz2) #TJ*J by 2
+  bXz=cbind(bXz1,bXz2)
+
+  xx<-polym(bX[,1],bX[,2],degree=L,raw=TRUE)
+  zz<-polym(bXz[,1],bXz[,2],degree=L,raw=TRUE)
+
+  basisX=matrix(NA,TJ,length(xx[1,]))
+  basisXz=matrix(NA,TJ,length(zz[1,]))
+
+
+  for (l in 1:TJ){
+    basisX[l,]=colSums(xx[(l*J-J+1):(l*J),]) #basisX is TJ by L
+    basisXz[l,]=colSums(zz[(l*J-J+1):(l*J),])
+  }
+
+
+  B=cbind(basisD,basisX)
+  IV=cbind(basisDz,basisXz)
+
+  y=R
+  X=B
+  Z=cbind(x,IV)
+  X=cbind(x,B)
+
+  #############IV 1####################
+  ginv_PP <- ginv(t(Z)%*%Z)
+  pz<- Z%*%ginv_PP%*%t(Z)
+  xhat<-pz%*%X
+
+  lm1<-lm(R~xhat -1)
+  fit2<-predict(lm1)
+  mean((R-fit2)^2)
+  coefficients(lm1)[1:2]
+
+  ##########IV 2#######################
+
+  ginv_PP <- ginv(t(IV)%*%IV)
+  pz<- IV%*%ginv_PP%*%t(IV)
+  Bhat<-pz%*%B
+  Xhat=cbind(x,Bhat)
+  lm2<-lm(R~Xhat -1)
+  fit2<-predict(lm2)
+  mean((R-fit2)^2)
+  beta0=coefficients(lm2)[1:2]
+
+  lambda_j.Alpha0hat <- .lambda_j.Alpha0hat(B, IV, sigma0_v, tune_type = "CV")
+  lambda_j <- lambda_j.Alpha0hat$lambda_j; Alpha0hat <- lambda_j.Alpha0hat$Alpha0hat
+  Dhat <- IV %*% Alpha0hat
+  XX=cbind(x,Dhat)
+  lambda.beta_Lasso <- .lambda.beta_Lasso(y, XX, sigma0_h, no_pen_ids = (1:2))
+  lambda <- lambda.beta_Lasso$lambda
+  beta_Lasso_XX <- lambda.beta_Lasso$beta_Lasso
+  beta1=beta_Lasso_XX[1:2]
+  #projected estimator
+  beta2=solve(t(x)%*%x)%*%t(x)%*%(y-XX[,3:ncol(XX)]%*%beta_Lasso_XX[3:length(beta_Lasso_XX)])
+  #delta_hat
+
+  xihat=y-XX%*%beta_Lasso_XX
+  deltahat=x%*%beta1+xihat
+  Deltahat=mean(delta-deltahat)
+
+  Betas=cbind(t(beta1),t(beta2))
+
+  A=cbind(Betas,Deltahat)
+
+  data.frame(
+    config_id = config_id_,
+    trial_id = trial_id,
+    beta1_1 = beta1[1],
+    beta1_2 = beta1[2],
+    beta2_1 = beta2[1],
+    beta2_2 = beta2[1],
+    Delta.hat = Deltahat
+    )
+}
+
+trial()
